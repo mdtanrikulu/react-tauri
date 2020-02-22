7fb0d3f18b165408a036cf8a58d7b5c3a4991cff
Muhammed Tanrikulu <md.tanrikulu@gmail.com>
2020-02-22 23:25:16 +0100

    Update icons, update codebase, add spring animation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+import clamp from 'lodash-es/clamp';
+import { useSpring, animated } from 'react-spring';
+import { useGesture } from 'react-with-gesture';
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [clicked, setClick] = useState(false);
+  const [{ xy }, set] = useSpring(() => ({ xy: [0, 0] }));
+  const bind = useGesture(({ down, delta, velocity }) => {
+    velocity = clamp(velocity, 1, 8);
+    set({
+      xy: down ? delta : [0, 0],
+      config: { mass: velocity, tension: 500 * velocity, friction: 50 }
+    });
+  });
+
+  const _onClick = _ => setClick(!clicked);
+
   return (
     <div className="App">
       <header className="App-header">
         <div className="overlay" />
-        <div className="author">
-          <span>React-Tauri</span>
+        <div className="sticker">
+          <animated.div
+            {...bind()}
+            style={{
+              transform: xy.interpolate(
+                (x, y) => `translate3d(${x}px,${y}px,0)`
+              )
+            }}>
+            <span onClick={_onClick}>
+              {!clicked ? 'React-Tauri' : 'Hellö!'}
+            </span>
+          </animated.div>
         </div>
         <div className="container">
           <svg
